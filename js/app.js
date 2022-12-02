da9b732064b6fcc9a68c79461c7e9066002f9bc0
John Doe <malachipito@protonmail.com>
2022-12-02 11:08:12 +0100

    Intégration de la librairie javascript AOS

--- js/app.js
+++ js/app.js
@@ -1,8 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let navigation = document.getElementById("team-presentation");
-    window.addEventListener("scroll", () => {
-        if(window.scrollY > 100){
-        navigation.classList.add("tracking-in-expand");}
-    });
-});
-
+AOS.init();
